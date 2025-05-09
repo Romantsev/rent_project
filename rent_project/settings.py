c435fb4ce031e6fcbe52f5265a3bffc1f864d102
Romantsev <romantsev505@gmail.com>
2025-05-09 17:15:20 +0300

    changes

--- rent_project/settings.py
+++ rent_project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rent_db',
         'USER': 'postgres',
-        'PASSWORD': 'your_password',
+        'PASSWORD': '77665544332211',
         'HOST': 'localhost',
         'PORT': '5432',
     }
